6be5aec5cb49e11c098ad7d9a5f68804ab703e33
Michal <developer239@users.noreply.github.com>
2021-06-22 13:54:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Mapbox Style Host
+
+## Deployment on Heroku
+
+This application is Heroku ready.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(cors())
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
