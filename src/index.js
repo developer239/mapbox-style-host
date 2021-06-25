fdfa0c9e788714cdc50ee7cf44c82df04e98ec72
michaljarnot <m.jarnot@yahoo.com>
2021-06-25 17:23:31 +0200

    feat: create storage proxy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const request = require('request')
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
@@ -10,6 +11,16 @@ app.get('/', (req, res) => {
 app.use(cors())
 
 app.use(express.static('public'))
+
+app.get("/proxy", (req, res) => {
+  const channel = req.query["channel"];
+  const date = req.query["date"];
+  const id = req.query["id"];
+
+  const targetUrl = `https://storage.googleapis.com/citizen-production/radio/channels/${channel}/${date}/${id}.aac`
+
+  request(targetUrl).pipe(res);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
